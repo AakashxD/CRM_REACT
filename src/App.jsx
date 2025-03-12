3f86356671eef818e85a38ade75dd7c108f67a36
Aakash <axkashsingh@gmail.com>
2025-03-12 13:53:28 +0530

    added daisy UI setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+   <>
+   <h2 className="text-amber-200"> hii </h2>
+   <button class="btn btn-primary">Button</button>
+   </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  purge: [],
+  darkMode: true, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
